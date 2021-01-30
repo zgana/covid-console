ba6e62ffab5f06f7dbd426957216086859f05c38
Michael Richman <mike.d.richman@gmail.com>
2021-01-30 14:49:54 -0500

    add citation components to repo

---
+++ src/components/charts/citations.js
@@ -0,0 +1,59 @@
+import React from 'react'
+
+import useLoad from '../data'
+
+
+const Citation = (props) => {
+  const { source, isLast } = props
+
+  console.log(source)
+  console.log(isLast)
+
+  return (
+    <span
+      style={{
+        display: `inline-block`,
+        padding: `0 .2em`,
+        // textAlign: `right`,
+        // listStylePosition: `inside`,
+      }}
+    >
+      <a href={source.url} target="_blank" rel="noopener noreferrer">{source.name}</a>
+      { isLast ? '' : ',' }
+    </span>
+  )
+}
+
+
+export default function Citations(props) {
+  const dataSpec = props.dataSpec
+  const { SOURCES } = useLoad(dataSpec)
+
+  const style = {
+    fontSize: `.75em`,
+    textAlign: `right`,
+    width: `95%`,
+    height: `.75em`,
+  }
+
+  if (!SOURCES) {
+    return (
+      <div style={style} >
+      </div>
+    )
+  }
+
+  const numSources = SOURCES.length
+
+  return (
+    <>
+      <div style={style} >
+        {numSources === 1 ? 'Source:' : 'Sources:'}
+
+        {Object.keys(SOURCES).map(i => (
+          <Citation source={SOURCES[i]} isLast={parseInt(i) === numSources - 1} />
+        ))}
+      </div>
+    </>
+  )
+}
