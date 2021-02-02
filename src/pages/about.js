1bdcc7a5a409ae2128ea9f24409d45e1556a93d3
Michael Richman <mike.d.richman@gmail.com>
2021-02-02 18:29:10 -0500

    link to console from about

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,8 @@
 import React from "react"
 import Layout from "../components/layout"
 
+import { Link } from 'gatsby'
+
 export default function About() {
   return (
     <Layout>
@@ -10,7 +12,7 @@ export default function About() {
         There are plenty of resources for inspecting the current status and history of the
         COVID-19 pandemic.  This is an effort (by a data scientist with very little prior
         experience with React and D3) to offer an interface to some specific trends I've
-        been following, plus a commandline-like interface on the homepage for "fun".
+        been following, plus a commandline-like <Link to="/console/">interface</Link> for "fun".
 
       </p>
     </Layout>
